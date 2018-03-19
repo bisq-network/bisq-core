0d205e28ffaa12817542a5cf0f72665149e00c15
Manfred Karrer <mk@nucleo.io>
2018-03-19 09:50:54 -0500

    Fix formatting of license header

--- src/test/java/bisq/core/payment/validation/AltCoinAddressValidatorTest.java
+++ src/test/java/bisq/core/payment/validation/AltCoinAddressValidatorTest.java
@@ -6,7 +6,9 @@
  * the Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
  *
- * Bisq is distributed in the hope that it will be useful, but WITHOUT * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
