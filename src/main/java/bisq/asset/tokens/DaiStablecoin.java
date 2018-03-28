f1d84c0b7b5e5a42718f93d5443de5368fb822b5
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-03-28 16:15:39 +0200

    Extract Dai Stablecoin

---
+++ src/main/java/bisq/asset/tokens/DaiStablecoin.java
@@ -0,0 +1,10 @@
+package bisq.asset.tokens;
+
+import bisq.asset.Erc20Token;
+
+public class DaiStablecoin extends Erc20Token {
+
+    public DaiStablecoin() {
+        super("Dai Stablecoin", "DAI");
+    }
+}
