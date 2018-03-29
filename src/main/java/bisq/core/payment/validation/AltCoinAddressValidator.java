686d134678a001badd164361af297fe1ffa6d966
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2018-03-29 10:14:11 +0200

    Extract Qwark

--- src/main/java/bisq/core/payment/validation/AltCoinAddressValidator.java
+++ src/main/java/bisq/core/payment/validation/AltCoinAddressValidator.java
@@ -84,11 +84,6 @@ public final class AltCoinAddressValidator extends InputValidator {
             }
 
             switch (currencyCode) {
-                case "QWARK":
-                    if (!input.matches("^(0x)?[0-9a-fA-F]{40}$"))
-                        return regexTestFailed;
-                    else
-                        return new ValidationResult(true);
                 case "GEO":
                     if (!input.matches("^(0x)?[0-9a-fA-F]{40}$"))
                         return regexTestFailed;
