a6cab79c1e8beee69789a61404f39dc1059b97c4
power7full <20664528+power7full@users.noreply.github.com>
2018-04-07 21:08:58 +0300

    Update MFCoin.java

--- src/main/java/bisq/asset/coins/MFCoin.java
+++ src/main/java/bisq/asset/coins/MFCoin.java
@@ -19,7 +19,7 @@ package bisq.asset.coins;
 import bisq.asset.Coin;
 import bisq.asset.RegexAddressValidator;
 
-public class Mfcoin extends Coin {
+public class MFCoin extends Coin {
     
     public Mfcoin(){
         super("MFCoin", "MFC", new RegexAddressValidator("^[M][a-km-zA-HJ-NP-Z1-9]{33}$"));
