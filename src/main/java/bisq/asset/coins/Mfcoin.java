fa6467cb4967b46b1e9e613164dfa37f7f2536a5
power7full <20664528+power7full@users.noreply.github.com>
2018-04-07 19:27:09 +0300

    List MFCoin (MFC)
    fix regexp

--- src/main/java/bisq/asset/coins/Mfcoin.java
+++ src/main/java/bisq/asset/coins/Mfcoin.java
@@ -5,6 +5,6 @@ import bisq.asset.RegexAddressValidator;
 
 public class Mfcoin extends Coin {
     public Mfcoin(){
-        super("MFCoin", "MFC", new RegexAddressValidator("^[M][a-km-zA-HJ-NP-Z1-9]{34}$"));
+        super("MFCoin", "MFC", new RegexAddressValidator("^[M][a-km-zA-HJ-NP-Z1-9]{33}$"));
     }
 }
