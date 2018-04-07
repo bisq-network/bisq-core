ddbf546340e2e2de2db81ef6eb18ebdf29deb7ca
power7full <20664528+power7full@users.noreply.github.com>
2018-04-07 21:03:34 +0300

    List MFCoin (MFC)
    Insert a single blank space after class declaration.

--- src/main/java/bisq/asset/coins/Mfcoin.java
+++ src/main/java/bisq/asset/coins/Mfcoin.java
@@ -20,6 +20,7 @@ import bisq.asset.Coin;
 import bisq.asset.RegexAddressValidator;
 
 public class Mfcoin extends Coin {
+    
     public Mfcoin(){
         super("MFCoin", "MFC", new RegexAddressValidator("^[M][a-km-zA-HJ-NP-Z1-9]{33}$"));
     }
