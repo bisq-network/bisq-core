28fd988d6bc49fb6c7bd48a8976ac0e09785ef89
LitecoinExtreme <37575975+ltcextreme@users.noreply.github.com>
2018-04-21 22:52:55 +0530

    Create LitecoinExtremeTest.java
    first commit from litecoinextreme

---
+++ src/test/java/bisq/asset/coins/LitecoinExtremeTest.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.asset.coins;
+
+import bisq.asset.AbstractAssetTest;
+
+import org.junit.Test;
+
+public class LitecoinExtremeTest extends AbstractAssetTest {
+
+    public LitecoinExtremeTest() {
+        super(new LitecoinExtreme());
+    }
+
+    @Test
+    public void testValidAddresses() {
+        assertValidAddress("EPQoqfEwpQn9NzrxrBfW1wN2tLQ1WvgfYs");
+        assertValidAddress("EWWWXsn83DEaiyKiWQM9PPwuBccd26SyFz");
+        assertValidAddress("EM4F4p2ZCiHh1dQCbqy15ZaQFaPPJkTUv3");
+        assertValidAddress("EdZxv4MpLurXyRyysvH756zKBAr5ujretk");
+    }
+
+    @Test
+    public void testInvalidAddresses() {
+        assertInvalidAddress("SxPZMBgDxBVDRisdDZGD1XQwyAz8RBbo3R");
+        assertInvalidAddress("miswBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn");
+        assertInvalidAddress("davvLuwjUrz2kf5gghEmUPFE3vFvwfEiL");
+        assertInvalidAddress(""); 
+    }
+}
