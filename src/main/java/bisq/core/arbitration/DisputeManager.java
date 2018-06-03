d29ffffbeb86244b17cea019feb98580c9e78742
Manfred Karrer <mk@nucleo.io>
2018-06-03 13:15:29 +0200

    Move onDisputesChangeListener to disputeManager

--- src/main/java/bisq/core/arbitration/DisputeManager.java
+++ src/main/java/bisq/core/arbitration/DisputeManager.java
@@ -65,6 +65,15 @@ import javax.inject.Named;
 
 import com.google.common.util.concurrent.FutureCallback;
 
+import org.fxmisc.easybind.EasyBind;
+import org.fxmisc.easybind.Subscription;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 import java.io.File;
@@ -73,6 +82,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -83,7 +93,11 @@ import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.Getter;
+
 import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public class DisputeManager implements PersistedDataHost {
     private static final Logger log = LoggerFactory.getLogger(DisputeManager.class);
@@ -104,6 +118,12 @@ public class DisputeManager implements PersistedDataHost {
     private final Map<String, Dispute> openDisputes;
     private final Map<String, Dispute> closedDisputes;
     private final Map<String, Timer> delayMsgMap = new HashMap<>();
+
+    private final Map<String, Subscription> disputeIsClosedSubscriptionsMap = new HashMap<>();
+    @Getter
+    private final StringProperty numOpenDisputesAsString = new SimpleStringProperty();
+    @Getter
+    private final BooleanProperty showOpenDisputesNotification = new SimpleBooleanProperty();
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////
@@ -181,6 +201,43 @@ public class DisputeManager implements PersistedDataHost {
         tryApplyMessages();
 
         cleanupDisputes();
+
+        disputes.getList().addListener((ListChangeListener<Dispute>) change -> {
+            change.next();
+            onDisputesChangeListener(change.getAddedSubList(), change.getRemoved());
+        });
+        onDisputesChangeListener(disputes.getList(), null);
+    }
+
+    private void onDisputesChangeListener(List<? extends Dispute> addedList, @Nullable List<? extends Dispute> removedList) {
+        if (removedList != null) {
+            removedList.forEach(dispute -> {
+                String id = dispute.getId();
+                if (disputeIsClosedSubscriptionsMap.containsKey(id)) {
+                    disputeIsClosedSubscriptionsMap.get(id).unsubscribe();
+                    disputeIsClosedSubscriptionsMap.remove(id);
+                }
+            });
+        }
+        addedList.forEach(dispute -> {
+            String id = dispute.getId();
+            Subscription disputeStateSubscription = EasyBind.subscribe(dispute.isClosedProperty(),
+                    isClosed -> {
+                        // We get the event before the list gets updated, so we execute on next frame
+                        UserThread.execute(() -> {
+                            int openDisputes = disputes.getList().stream()
+                                    .filter(e -> !e.isClosed())
+                                    .collect(Collectors.toList()).size();
+                            if (openDisputes > 0)
+                                numOpenDisputesAsString.set(String.valueOf(openDisputes));
+                            if (openDisputes > 9)
+                                numOpenDisputesAsString.set("★");
+
+                            showOpenDisputesNotification.set(openDisputes > 0);
+                        });
+                    });
+            disputeIsClosedSubscriptionsMap.put(id, disputeStateSubscription);
+        });
     }
 
     public void cleanupDisputes() {
